6f7e483d41b979a1aead3aeea50b4af6366bb1ab
aleanse <aleanse_54@hotmail.com>
2026-06-11 13:21:48 -0300

    teste deploy 2

--- project/settings.py
+++ project/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 ]
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE =  "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
